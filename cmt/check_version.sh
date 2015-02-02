c3752bb831d91a5c56574204f00c335d57993be4
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-02-02 17:15:32 +0900

    Fix the regular expressions in the version check.

--- cmt/check_version.sh
+++ cmt/check_version.sh
@@ -9,21 +9,21 @@ if [ ! -f version.cmt ]; then
     return
 fi
 
-if grep master version.cmt; then
+if grep master version.cmt >> /dev/null; then
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     echo % Development version of captainRelease is being used.
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     return
 fi
 
-if grep .999 version.cmt; then
+if grep '\.999' version.cmt >> /dev/null; then
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     echo % Development version of captainRelease is being used.
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     return
 fi
 
-if ! grep '\.[0-9]+' version.cmt; then
+if ! grep '\.[0-9]\+$' version.cmt >> /dev/null; then
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     echo % Development version of captainRelease is being used.
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
