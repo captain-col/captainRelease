4c34456335f1b53fcd95a38aa086ebf8bc7638e7
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-02-02 17:04:58 +0900

    Add script to warn of a development version.

--- cmt/check_version.sh
+++ cmt/check_version.sh
@@ -6,26 +6,26 @@ if [ ! -f version.cmt ]; then
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     echo % The version file version.cmt is missing
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    exit 0
+    return
 fi
 
 if grep master version.cmt; then
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     echo % Development version of captainRelease is being used.
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    exit 0
+    return
 fi
 
 if grep .999 version.cmt; then
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     echo % Development version of captainRelease is being used.
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    exit 0
+    return
 fi
 
 if ! grep '\.[0-9]+' version.cmt; then
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     echo % Development version of captainRelease is being used.
     echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    exit 0
+    return
 fi
